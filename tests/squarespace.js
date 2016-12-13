5bd108acaecfce37cf068e617ce4c74fe06f8b17
Laurence Cruz <lcruz@squarespace.com>
2016-12-12 19:26:43 -0500

    [squarespace.js] Adds basic test case for squarespace

---
+++ tests/squarespace.js
@@ -0,0 +1,22 @@
+// google_search.js
+var webdriver = require('selenium-webdriver'),
+    By = webdriver.By;
+
+
+// instantiate browser object
+// NOTE: Only Firefox will work
+var driver = new webdriver.Builder()
+    .forBrowser('firefox')
+    .build();
+
+
+// start doing testing stuffs
+driver.get('http://www.squarespace.com');
+
+driver.sleep(5000);
+
+driver.findElement(By.css('.get-started')).click();
+
+driver.sleep(5000);
+
+driver.quit();
