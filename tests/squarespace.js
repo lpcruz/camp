f61fbe0c70b62a753ccfd3b710e05e280b5cb15f
Laurence Cruz <lcruz@squarespace.com>
2016-12-13 12:32:02 -0500

    [squarespace.js] Clean test and add ability for screenshot assertions

--- tests/squarespace.js
+++ tests/squarespace.js
@@ -1,22 +1,40 @@
 // squarespace.js
 var webdriver = require('selenium-webdriver'),
-    By = webdriver.By;
-
-
-// instantiate browser object
-// NOTE: Only Firefox will work
-var driver = new webdriver.Builder()
+    ByCssSelector = webdriver.By.css;
+    url = 'http://www.squarespace.com';
+    browser = new webdriver.Builder()
     .forBrowser('firefox')
     .build();
 
+/**
+ * TEST: Go to Squarespace homepage and click the 'Get Started' Button
+ * AUTHOR: lcruz
+ */
 
-// start doing testing stuffs
-driver.get('http://www.squarespace.com');
+// 1. Go to the url
+browser.get(url);
+browser.sleep(5000);
 
-driver.sleep(5000);
+// 2. Take screenshot of homepage
+browser.takeScreenshot().then(
+    function(image) {
+        require('fs-path').writeFileSync('./screens/squarespace/homepage.png', image, 'base64');
+        console.log('✔  Successfully took a screenshot of Squarespace homepage');
+    }
+);
+browser.sleep(5000);
 
-driver.findElement(By.css('.get-started')).click();
+// 3. Click the 'Get Started' Button
+browser.findElement(ByCssSelector('.get-started')).click();
+browser.sleep(5000);
 
-driver.sleep(5000);
+// 4. Take a screenshot after clicking the 'Get Started' Button
+browser.takeScreenshot().then(
+    function(image) {
+        require('fs-path').writeFileSync('./screens/squarespace/get-started.png', image, 'base64');
+        console.log('✔  Successfully took a screenshot of Get Started Page');
+    }
+);
 
-driver.quit();
+// 5. Close browser
+browser.quit();
