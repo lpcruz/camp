6532cfa66a49b65b71a9d18e3a8c16a887fe5852
Laurence Cruz <lcruz@squarespace.com>
2016-12-14 11:57:40 -0500

    Use app-root-dir module to automatically open screens directory

--- tests/squarespace.js
+++ tests/squarespace.js
@@ -1,12 +1,15 @@
 // squarespace.js
 var webdriver = require('selenium-webdriver'),
-    util = require('util'),
+    open = require('open'),
     fsPath = require('fs-path'),
+    appRootDir = require('app-root-dir').get(),
     ByCssSelector = webdriver.By.css,
     url = 'http://www.squarespace.com',
     browser = new webdriver.Builder()
     .forBrowser('firefox')
     .build();
+
+require('app-root-dir').set(__dirname);
 
 /**
  * TEST: Go to Squarespace homepage and click the 'Get Started' Button
@@ -20,7 +23,7 @@ browser.sleep(5000);
 // 2. Take screenshot of homepage
 browser.takeScreenshot().then(
     function(image) {
-        fsPath.writeFileSync('./screens/squarespace/homepage.png', image, 'base64');
+        fsPath.writeFileSync(appRootDir + '/screens/squarespace/homepage.png', image, 'base64');
         console.log('✔  Successfully took a screenshot of Squarespace homepage');
     }
 );
@@ -33,11 +36,11 @@ browser.sleep(5000);
 // 4. Take a screenshot after clicking the 'Get Started' Button
 browser.takeScreenshot().then(
     function(image) {
-        require('fs-path').writeFileSync('./screens/squarespace/get-started.png', image, 'base64');
+        require('fs-path').writeFileSync(appRootDir + '/screens/squarespace/get-started.png', image, 'base64');
         console.log('✔  Successfully took a screenshot of Get Started Page');
+        open(appRootDir + '/screens/squarespace/', 'finder');
     }
 );
 
 // 5. Close browser
 browser.quit();
-util.format('open ./screens/squarespace');
